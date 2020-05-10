0222c5d310ccdbe53c86922d904cd643ce769620
Scott  Keplinger <skeplin17@gmail.com>
2020-05-10 20:59:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddie Mercury"
       contact_details_hash.each do |attribute, data|
-    binding.pry
+         binding.pry
   end
   end
 end
